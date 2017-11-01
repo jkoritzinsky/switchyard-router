a96cff93ffaa87e1523d0e3ac4ed9885f7a1b325
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-11-01 11:36:30 -0500

    Load a forwarding table from a file.

--- myrouter.py
+++ myrouter.py
@@ -7,12 +7,17 @@ Basic IPv4 router (static routing) in Python.
 import sys
 import os
 import time
+import csv
+from collections import namedtuple
 from switchyard.lib.userlib import *
 
+ForwardingEntry = namedtuple("ForwardingEntry", ["network", "next_hop", "interface"], verbose=True)
+
 class Router(object):
-    def __init__(self, net):
+    def __init__(self, net, table):
         self.net = net
         self.arp_cache = {}
+        self.forwarding_table = list(table)
         # other initialization stuff here
 
 
@@ -54,11 +59,21 @@ class Router(object):
         self.arp_cache[ipaddr] = ethaddr
 
 
+def create_forwarding_table(filename):
+    with open(filename, 'rt') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ')
+        for row in reader:
+            row = [entry for entry in row if entry]
+            if not row:
+                continue
+            log_debug("Entry {}/{} on interface {}".format(row[0], row[1], row[2]))
+            yield ForwardingEntry(IPv4Network("{}/{}".format(row[0], row[1])), IPv4Address(row[2]), row[3])
+
 def main(net):
     '''
     Main entry point for router.  Just create Router
     object and get it going.
     '''
-    r = Router(net)
+    r = Router(net, create_forwarding_table('forwarding_table.txt'))
     r.router_main()
     net.shutdown()
